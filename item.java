cf7f5cd5376b00c0cb71897b31bddb51a7bb3f77
gavinfootball5 <114198957+gavinfootball5@users.noreply.github.com>
2024-08-16 00:48:00 -0500

    Update item.java

--- item.java
+++ item.java
@@ -2,3 +2,13 @@ public class Item {
 private String name;
 private String description;
 }
+public Item(String name, String description){
+  this.name = name;
+  this.description = description;
+}
+
+public String getName() {
+  return name;
+  public String getDescription(){
+    return description;
+  }
